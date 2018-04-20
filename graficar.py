4bea0e1fd81906c7b4dfc6739640d654e9fab4e3
Fer Frassia <ferfrassia@gmail.com>
2018-04-20 02:54:23 -0300

    arme unos graficos

---
+++ graficar.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+
+def parsear(fileName):
+	with open(fileName) as f:
+	    content = f.readlines()
+	fileList = [x.strip() for x in content]
+
+	threads = []
+	tiempo = []
+	for results in fileList:
+		threads.append(int((results.split(" "))[0]))
+		tiempo.append(int((results.split(" "))[1]))
+
+	return {"threads":threads, "tiempo":tiempo}
+
+
+def graficarAyB(a, b, labelA, labelB, titulo, x, y):
+	d5 = parsear(a)
+	d6 = parsear(b)
+
+	plt.plot(d5["threads"], d5["tiempo"], "ro", label=labelA)
+	plt.plot(d6["threads"], d6["tiempo"], "b", label=labelB)
+	plt.axis([0, max(d5["threads"]+d6["threads"]), 0, max(d5["tiempo"]+d6["tiempo"])])
+	plt.title(titulo)
+	plt.xlabel(x)
+	plt.ylabel(y)
+	plt.show()
+
+
+graficarAyB("maximum5-50-50","maximum6-50-50", "Maximum 5", "Maximum 6", "Maximum 5 y 6", "Threads", "Tiempo");
+graficarAyB("countWords1-40-30","countWords2-40-30", "Count Words 1", "Count Words 2","Count Words 1 y 2", "Threads", "Tiempo")
--- main.cpp
+++ main.cpp
@@ -7,10 +7,117 @@
 using namespace std;
 
 
+void testsCountWords1(int cantArch, int cantThreads,string salida ){
+		ofstream txt(salida);
+
+	list<string> l ;
+	for(int i=0;i<cantArch;i++){
+		l.push_front("Corpuses/corpus" + to_string(i));
+	}
+	unsigned long delta=1111111110;
+	for(int i=0;i<100;i++){
+		unsigned long start, end;
+		RDTSC_START(start);
+		ConcurrentHashMap* h = ConcurrentHashMap::count_words(l);
+		RDTSC_STOP(end);
+		delete h;
+		delta = (end - start) < delta ? (end - start) : delta;
+	}
+	txt<<(delta)<<endl;
+	ConcurrentHashMap* h = ConcurrentHashMap::count_words(1,l);
+
+	delta=1111111110;
+	txt.close();
+}
+
+void testsCountWords2(int cantArch, int cantThreads,string salida ){
+		ofstream txt(salida);
+
+	list<string> l ;
+	for(int i=0;i<cantArch;i++){
+		l.push_front("Corpuses/corpus" + to_string(i));
+	}
+	unsigned long delta=1111111110;
+	delta=1111111110;
+
+	for(int j=1;j<cantThreads;j++){
+		delta=1111111110;
+		for(int i=0;i<100;i++){
+			unsigned long start, end;
+			RDTSC_START(start);
+			ConcurrentHashMap* h = ConcurrentHashMap::count_words(j,l);
+			RDTSC_STOP(end);
+			delete h;
+			delta = (end - start) < delta ? (end - start) : delta;
+		}
+		txt<<j<<" "<<(delta)<<endl;
+	}
+
+	txt.close();
+}
+
+
+void test1() {
+	testsCountWords1(40,30,"countWords1-40-30");
+}
+
+void testsMaximum5(int cantArch, int cantThreads,string salida ){
+		ofstream txt(salida);
+
+	list<string> l ;
+	for(int i=0;i<cantArch;i++){
+		l.push_front("Corpuses/corpus" + to_string(i));
+	}
+
+	unsigned long delta=1111111110;
+	delta=1111111110;
+
+	for(int j=1;j<cantThreads+1;j++){
+		delta=1111111110;
+		for(int i=0;i<100;i++){
+			unsigned long start, end;
+			RDTSC_START(start);
+			ConcurrentHashMap::maximum5(j,cantArch,l);
+			RDTSC_STOP(end);
+			//delta += end - start;
+			delta = (end - start) < delta ? (end - start) : delta;
+		}
+		txt<<j<<" "<<(delta)<<endl;
+	}
+
+	delta=1111111110;
+
+	txt.close();
+}
+
+void testsMaximum6(int cantArch, int cantThreads,string salida ){
+		ofstream txt(salida);
+
+	list<string> l ;
+	for(int i=0;i<cantArch;i++){
+		l.push_front("Corpuses/corpus" + to_string(i));
+	}
+	unsigned long delta=1111111110;
+	delta=1111111110;
+
+	for(int j=1;j<cantThreads+1;j++){
+		delta=1111111110;
+		for(int i=0;i<100;i++){
+			unsigned long start, end;
+			RDTSC_START(start);
+			ConcurrentHashMap::maximum6(j,cantArch,l);
+			RDTSC_STOP(end);			//delta += end - start;
+			delta = (end - start) < delta ? (end - start) : delta;
+		}
+		txt<<j<<" "<<(delta)<<endl;
+	}
+
+	delta=1111111110;
+	txt.close();
+}
+
 void test2() {
-	ConcurrentHashMap* h = ConcurrentHashMap::count_words("corpus");
-	ConcurrentHashMap::printConcurrentHashMap(h);
-	delete h;
+	testsCountWords2(40,30,"countWords2-40-30");
 }
 
 void test3() {
@@ -31,109 +138,11 @@ void test4() {
 }
 
 void test5() {
-
-}
-
-void testsCountWords(int cantArch, int cantThreads,string salida ){
-		ofstream txt(salida);
-
-	list<string> l ;
-	for(int i=0;i<cantArch;i++){
-		l.push_front("Corpuses/corpus" + to_string(i));
-	}
-	unsigned long delta=1111111110;
-	for(int i=0;i<100;i++){
-		unsigned long start, end;
-		RDTSC_START(start);
-		ConcurrentHashMap* h = ConcurrentHashMap::count_words(l);
-		RDTSC_STOP(end);
-		delete h;
-		//delta += end - start;
-		delta = (end - start) < delta ? (end - start) : delta;
-		cout<<"vuelta "<<i<<" del 1-1"<<endl;
-	}
-	txt<<"El count_words de 1 thread - 1 archivo tomo tiempo min: "<<(delta)<<endl;
-	cout<<"ahora pasamos al siguiente pero primero probamos que onda..."<<endl;
-	ConcurrentHashMap* h = ConcurrentHashMap::count_words(1,l);
-	cout<<"ahora si probamos posta"<<endl;
-
-	delta=1111111110;
-
-	for(int j=1;j<cantThreads;j++){
-		cout<<"iteracion de "<<j<<"threads"<<endl;
-		delta=1111111110;
-		for(int i=0;i<100;i++){
-			unsigned long start, end;
-			RDTSC_START(start);
-			ConcurrentHashMap* h = ConcurrentHashMap::count_words(j,l);
-			RDTSC_STOP(end);
-			delete h;
-			//delta += end - start;
-			delta = (end - start) < delta ? (end - start) : delta;
-			cout<<"vuelta "<<i<<" del "<<j<<" threads"<<endl;
-		}
-		txt<<"El count_words de "<<j<<" threads tomo tiempo min: "<<(delta)<<endl;
-	}
-
-	txt.close();
-}
-
-
-void test1() {
-	testsCountWords(40,30,"test count_words 40 archivos 30threads");
-}
-
-void testsMaximum(int cantArch, int cantThreads,string salida ){
-		ofstream txt(salida);
-
-	list<string> l ;
-	for(int i=0;i<cantArch;i++){
-		l.push_front("Corpuses/corpus" + to_string(i));
-	}
-	unsigned long delta=1111111110;
-	delta=1111111110;
-
-
-
-	for(int j=1;j<cantThreads+1;j++){
-		cout<<"iteracion de "<<j<<"threads maximum6"<<endl;
-		delta=1111111110;
-		for(int i=0;i<100;i++){
-			unsigned long start, end;
-			RDTSC_START(start);
-			ConcurrentHashMap::maximum6(j,cantArch,l);
-			RDTSC_STOP(end);			//delta += end - start;
-			delta = (end - start) < delta ? (end - start) : delta;
-			cout<<"vuelta "<<i<<" del "<<j<<" threads maximum6"<<endl;
-		}
-		txt<<"El maximum6 de "<<j<<" threads tomo tiempo min: "<<(delta)<<endl;
-	}
-
-
-	for(int j=1;j<cantThreads+1;j++){
-		cout<<"iteracion de "<<j<<"threads maximum5"<<endl;
-		delta=1111111110;
-		for(int i=0;i<100;i++){
-			unsigned long start, end;
-			RDTSC_START(start);
-			ConcurrentHashMap::maximum5(j,cantArch,l);
-			RDTSC_STOP(end);
-			//delta += end - start;
-			delta = (end - start) < delta ? (end - start) : delta;
-			cout<<"vuelta "<<i<<" del "<<j<<" threads maximum5"<<endl;
-		}
-		txt<<"El maximum5 de "<<j<<" threads tomo tiempo min: "<<(delta)<<endl;
-	}
-
-	delta=1111111110;
-
-
-
-	txt.close();
+	testsMaximum5(50,50,"maximum5-50-50");
 }
 
 void test6() {
-	testsMaximum(50,50,"test maximum 50 archivos y 50 threads");
+	testsMaximum6(50,50,"maximum6-50-50");
 }
 
 int main(int argc, char * argv[]) {
