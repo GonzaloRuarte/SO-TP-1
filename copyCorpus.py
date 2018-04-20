a81a3d9dcd65d4dd10b87b6c909b5bd810db60fd
Sorongoku <gonzalorpg@gmail.com>
2018-04-20 13:50:17 -0300

    we are doomed

--- copyCorpus.py
+++ copyCorpus.py
@@ -1,9 +1,16 @@
 import os
 import shutil
 import datetime
+import random
 
 
 for i in range(0,50):
 	src_dir="corpus"
 	dst_dir="Corpuses/corpus" + str(i)
-	shutil.copy(src_dir,dst_dir)
+	with open(src_dir, 'r') as source:
+		data = [ ( random.random(),line) for line in source ]
+	data.sort()
+	with open(dst_dir, 'w') as target:
+		for _, line in data:
+			target.write( line )	
+	
