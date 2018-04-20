c97e7c2c2f7fbad8608ae9f536665ee972bd4498
Fer Frassia <ferfrassia@gmail.com>
2018-04-20 19:51:19 -0300

    cambio graficos por puntos

--- graficar.py
+++ graficar.py
@@ -18,8 +18,8 @@ def graficarAyB(a, b, labelA, labelB, titulo, x, y):
 	d5 = parsear(a)
 	d6 = parsear(b)
 
-	plt.plot(d5["threads"], d5["tiempo"], "ro", label=labelA)
-	plt.plot(d6["threads"], d6["tiempo"], "b", label=labelB)
+	plt.plot(d5["threads"], d5["tiempo"], "r--", label=labelA)
+	plt.plot(d6["threads"], d6["tiempo"], "g--", label=labelB)
 	plt.axis([0, max(d5["threads"]+d6["threads"]), 0, max(d5["tiempo"]+d6["tiempo"])])
 	plt.title(titulo)
 	plt.xlabel(x)
@@ -27,5 +27,5 @@ def graficarAyB(a, b, labelA, labelB, titulo, x, y):
 	plt.show()
 
 
-graficarAyB("maximum5-50-50","maximum6-50-50", "Maximum 5", "Maximum 6", "Maximum 5 y 6", "Threads", "Tiempo");
+graficarAyB("maximum5-50-50","maximum6-50-50", "Maximum 5", "Maximum 6", "Maximum 5 y 6", "Threads", "Tiempo")
 graficarAyB("countWords1-40-30","countWords2-40-30", "Count Words 1", "Count Words 2","Count Words 1 y 2", "Threads", "Tiempo")
